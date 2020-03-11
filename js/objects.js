666cd33b1d836a3a36e9df44124207beff81384a
PawleN-D <sirpawle@gmail.com>
2020-03-11 11:58:42 +0200

    added a few more examples for OOP methods

--- js/objects.js
+++ js/objects.js
@@ -10,41 +10,87 @@
 // console.log(car.brand);
 // console.log(car.color);
 // console.log("He was driving a " + car.color + " " +car.brand + " when i saw him.");
+// const cake = {
+//     flavour: "Red Velvet",
+//     layers: "3",
+//     price: "R250",
+//     occasion: "farewell"
+// }
+
+// console.log(cake.flavour);
+// console.log(cake.layers);
+// console.log(cake.occasion);
+// console.log(cake.price);
+// // console.log("He got a " + cake.layers + " layered " + cake.flavour + " cake for his " + cake.occasion + ". It is said to be priced at " + cake.price);
+// function describe(cake) {
+//     let description = "He got a " + cake.layers + " layered " + cake.flavour + " cake for his " + cake.occasion + ". It is said to be priced at " + cake.price + ".";
+//     return description;
+// }
+
+// console.log(describe(cake));
+
+// // New Values
+// console.log(" ")
+// cake.occasion = "anniversary";
+// cake.layers = 3 + 3;
+// cake.price = "R550";
+
+// console.log(cake.flavour);
+// console.log(cake.layers);
+// console.log(cake.occasion);
+// console.log(cake.price);
+
+// console.log("He got a " + cake.layers + " layered " + cake.flavour + " cake for his " + cake.occasion + ". It is said to be priced at " + cake.price);
+
+// Object Method First Approach
+
+// function describe(cake) {
+//     let description = "He got a " + cake.layers + " layered " + cake.flavour + " cake for his " + cake.occasion + ". It is said to be priced at " + cake.price + ".";
+//     return description;
+// }
+
+// console.log(describe(cake));
+
+// Object Method Second Approach (recommended)
 const cake = {
     flavour: "Red Velvet",
     layers: "3",
     price: "R250",
-    occasion: "farewell"
+    occasion: "farewell",
+    describe: function() {
+        let description = "He got a " + cake.layers + " layered " + cake.flavour + " cake for his " + cake.occasion + ". It is said to be priced at " + cake.price + ".";
+        return description;
+    }
 }
 
-console.log(cake.flavour);
-console.log(cake.layers);
-console.log(cake.occasion);
-console.log(cake.price);
-// console.log("He got a " + cake.layers + " layered " + cake.flavour + " cake for his " + cake.occasion + ". It is said to be priced at " + cake.price);
-function describe(cake) {
-    let description = "He got a " + cake.layers + " layered " + cake.flavour + " cake for his " + cake.occasion + ". It is said to be priced at " + cake.price + ".";
-    return description;
-}
+console.log(cake.describe());
 
-console.log(describe(cake));
+console.log(" ");
 
-// New Values
-console.log(" ")
-cake.occasion = "anniversary";
-cake.layers = 3 + 3;
-cake.price = "R550";
+// New Object Method Example
 
-console.log(cake.flavour);
-console.log(cake.layers);
-console.log(cake.occasion);
-console.log(cake.price);
 
-// console.log("He got a " + cake.layers + " layered " + cake.flavour + " cake for his " + cake.occasion + ". It is said to be priced at " + cake.price);
+// Object Methods Example
 
-function describe(cake) {
-    let description = "He got a " + cake.layers + " layered " + cake.flavour + " cake for his " + cake.occasion + ". It is said to be priced at " + cake.price + ".";
-    return description;
-}
+let student = {
+    name: prompt("Enter name"),
+    faculty: prompt("Enter Faculty"),
+    year: prompt("Enter year of study"),
+    campus: prompt("Enter campus"),
+    organisation: prompt("Enter Organisation registered"),
+    status: function(){
+        let description = this.name + " is enrolled in the " + this.faculty + " faculty, doing his " + this.year + " year at the " + this.campus + " campus. They are part of " + this.organisation + " organisation.";
+        return description;
+    }
 
-console.log(describe(cake));
+};
+
+console.log(student.name);
+console.log(student.faculty);
+console.log(student.year);
+console.log(student.campus);
+console.log(student.organisation);
+console.log(" ");
+console.log(student.status());
+
+
