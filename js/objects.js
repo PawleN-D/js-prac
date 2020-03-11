ab4fa622e278ce04ac1fab61cc0f0fd65c696d63
PawleN-D <sirpawle@gmail.com>
2020-03-11 10:44:27 +0200

    examples of objects

--- js/objects.js
+++ js/objects.js
@@ -1,11 +1,38 @@
 // Object Oriented Programming
 
-let car = {
-    model: "2017",
-    brand: "BMW",
-    color: "black"
+// let car = {
+//     model: "2017",
+//     brand: "BMW",
+//     color: "black"
+// }
+
+// console.log(car.model);
+// console.log(car.brand);
+// console.log(car.color);
+// console.log("He was driving a " + car.color + " " +car.brand + " when i saw him.");
+const cake = {
+    flavour: "Red Velvet",
+    layers: "3",
+    price: "R250",
+    occasion: "farewell"
 }
 
-console.log(car.model);
-console.log(car.brand);
-console.log(car.color);
+console.log(cake.flavour);
+console.log(cake.layers);
+console.log(cake.occasion);
+console.log(cake.price);
+console.log("He got a " + cake.layers + " layered " + cake.flavour + " cake for his " + cake.occasion + ". It is said to be priced at " + cake.price);
+
+// New Values
+console.log(" ")
+cake.occasion = "anniversary";
+cake.layers = 3 + 3;
+cake.price = "R550";
+
+console.log(cake.flavour);
+console.log(cake.layers);
+console.log(cake.occasion);
+console.log(cake.price);
+
+console.log("He got a " + cake.layers + " layered " + cake.flavour + " cake for his " + cake.occasion + ". It is said to be priced at " + cake.price);
+
